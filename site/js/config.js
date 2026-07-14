9c408b4da474785d7383a051c627f52cc227dcf0
Nikatseplyaeva <57149024+Nikatseplyaeva@users.noreply.github.com>
2026-07-14 16:12:19 +0300

    Delete site directory

--- site/js/config.js
+++
@@ -1,4 +0,0 @@
-// Telegram bot credentials for RSVP notifications.
-// See ../telegram-bot/README.md for setup steps.
-window.TELEGRAM_BOT_TOKEN = '8859443090:AAGnkTd0JeMyEysgXnvHREiW9CpFVejqADs';
-window.TELEGRAM_CHAT_ID = '1975330210';
